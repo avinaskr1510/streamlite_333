d62aff17aa7b7b0e9e776756161bc974a3d9fb8c
avinaskr1510 <52924233+avinaskr1510@users.noreply.github.com>
2025-06-04 21:37:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.subheader("Brewing streamlit")
 st.text(" back to text")
 st.write("hekoo")
 
-chai=st.selectbox("Your fav chai",tea,abcmrelable,taaza,brook)
+chai=st.selectbox("Your fav chai",['tea',redlab0le','taaza','brook'])
 st.write("your fav chai {chai} , excellent choice")
 st.success("Select wisely")
 
