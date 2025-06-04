29e5c7b066ce867ede33c4c599de43f50302be9e
avinaskr1510 <52924233+avinaskr1510@users.noreply.github.com>
2025-06-04 21:28:54 +0530

    selectbox_success

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ st.title("Hello Chai App")
 st.subheader("Brewing streamlit")
 st.text(" back to text")
 st.write("hekoo")
+
+chai=st.selectbox("Your fav chai",tea,abcmrelable,taaza,brook)
+st.write("your fav chai {chai} , excellent choice")
+st.success("Select wisely")
+
